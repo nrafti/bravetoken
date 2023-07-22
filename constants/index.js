8cb82f76d85d254137e58a9d6a347b0aa95f58dc
nrafti <140282669+nrafti@users.noreply.github.com>
2023-07-23 01:41:49 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa0f2eb7a7aa83f7d051805777329cf1d76948d6c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
